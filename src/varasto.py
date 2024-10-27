fae283690528df2f92cce7b0f1f0b4d37cccff96
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-10-27 18:06:54 +0200

    Test pylint in Actions

--- src/varasto.py
+++ src/varasto.py
@@ -16,6 +16,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
+    # tämä rivi on pylintin mielestä aivan liian pitkä. Viikon 2 tehtävä 4 GitHub Actions testi.
     # huom: ominaisuus voidaan myös laskea.
     # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
