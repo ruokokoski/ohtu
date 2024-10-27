dfbec29be61023b3cbc1610011f56e525b82eb52
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-10-27 18:09:13 +0200

    Ex2-4: fix lint

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,6 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # tämä rivi on pylintin mielestä aivan liian pitkä. Viikon 2 tehtävä 4 GitHub Actions testi.
     # huom: ominaisuus voidaan myös laskea.
     # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
